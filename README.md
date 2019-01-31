80fcae870a9f930a63b3141d5091f589bbba5876
Dimitrije Radojević <dimitrijer@nordeus.com>
2019-02-01 00:40:06 +0100

    Add armbian config files

--- README.md
+++ README.md
@@ -18,10 +18,11 @@ Armbian has a really powerfull set of [build](https://github.com/armbian/build)
 tools, and they also support OPZ out of the box. Vagrant build process is well
 documented and keeps all the mess within builder VM.
 
-I selected `next` kernel with default configuration. In `config-default.conf`,
-I specified a separate 100M FAT boot partition, which allows me to read SD card
-on Windows/Linux/Mac machines (FAT partition needs to be the first primary
-partition on SD card for Windows to read it).
+I selected `next` kernel with default configuration, and I selected Debian
+Stretch as image basis. In `config-default.conf`, I specified a separate 100M
+FAT boot partition, which allows me to read SD card on Windows/Linux/Mac
+machines (FAT partition needs to be the first primary partition on SD card for
+Windows to read it).
 
 I also have a custom script called `customize-image.sh`, which should be placed
 in `userpatches` folder before building the image. This script is called in
---
+++ armbian/customize-image.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# arguments: $RELEASE $LINUXFAMILY $BOARD $BUILD_DESKTOP
+#
+# This is the image customization script
+
+# NOTE: It is copied to /tmp directory inside the image
+# and executed there inside chroot environment
+# so don't reference any files that are not already installed
+
+# NOTE: If you want to transfer files between chroot and host
+# userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
+
+RELEASE=$1
+LINUXFAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
+Main() {
+	case $RELEASE in
+		jessie)
+			# your code here
+			;;
+		xenial)
+			# your code here
+			;;
+		stretch)
+			# your code here
+            SetupSensor
+			;;
+	esac
+} # Main
+
+SetupSensor() {
+    echo "alpha" > /boot/id.txt
+    echo "network={
+ ssid=\"Foundation\"
+ psk=\"mijedobiogrip\"
+ #key_mgmt=NONE
+}" > /boot/wpa.txt
+
+    echo "allow-hotplug eth0                                  
+no-auto-down eth0                                                                         
+iface eth0 inet dhcp" > /etc/network/interfaces.d/eth0
+
+    echo "auto wlan0
+allow-hotplug wlan0
+iface wlan0 inet dhcp
+    wpa-conf /boot/wpa.txt" > /etc/network/interfaces.d/wlan0
+
+    echo "auto lo
+iface lo inet loopback" > /etc/network/interfaces.d/lo
+
+    echo "source /etc/network/interfaces.d/*" > /etc/network/interfaces
+    systemctl disable NetworkManager.service
+}
+
+Main "$@"
+
